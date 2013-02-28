0fbfca00c0648794fcd0edaffc8d5e9c296fbac2
jenshane <jmbshane@gmail.com>
2013-02-27 23:44:48 -0500

    Update recognize_gesture.c

--- recognize_gesture.c
+++ recognize_gesture.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int** recognize(int *array) {
-	int** holder = malloc(sizeof(int*)*2);
-	int avg[11];
-	int *hold = calloc(sizeof(int)*11);
+int* recognize(int *array, int* avg) {
+	int j,k;
 	for (j=0;j<11;j++) {
 		array[j] += hold[j]; 
 	}
 
 		for (k=0;k<11;k++) {
-			avg[k] = 5*hold[k] + somearray[i].array[k];
+			avg[k] = (5*avg[k] + array[k])/6;
 		}
 	}
-
-	holder[0] = avg;
-	holder[1] = array;
+	for(k=0;k<11;k++)
+	{
+		if(abs(avg[k]-array[k])>3)
+		{
+			
+			return avg;
+		}
+		
+	}
+	isAction(peter,avg);
 	return avg;
 }
