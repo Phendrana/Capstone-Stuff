38ebbf17877f4ed534fe1cd211e848a0b9a5cedc
marsh242 <hockey.marsh@yahoo.com>
2013-03-11 23:04:34 -0300

    Update notmain.c

--- notmain.c
+++ notmain.c
@@ -34,13 +34,69 @@ int b1=7;//mux digital pin #1
 int b0=8;//mux digital pin #2
 int* p;//recognize() data
 int readings[11];//data from sensors
+int * dims;//ordered list of dimentions
+int * crosses;//ordered list of crosses in given dimention
+
+
+
+//@inputs action that was just added and weight to be given to it(mostly -1/1)
+//finds the number of crosses created by a specific action adds them to crosses
+//and sorts dims based on crosses
+void dimShift(struct actionNode * new,int weight)
+{
+	struct actionNode *head=peter;
+  	int i,j,min,minat,temp;
+	if(dims==NULL)
+	{
+		dims=malloc(sizeof(int)*11);
+		for(i=0;i<11;i++)
+		{
+			dims[i]=i;
+		}
+		crosses=calloc(11,sizeof(int));
+	}
+	while(head!=NULL)
+	{
+		if(head==new)
+		{
+			head=head->next;
+			continue;
+		}
+		for(i=0;i<11;i++)
+		{
+			if((head->action->high[i]<new->action->low[i])||((head->action->low[i]>new->action->high[i]))
+			{
+				crosses[dims[i]]=crosses[dims[i]]+weight;
+			}
+		}
+		head=head->next;
+	}
+	for(i=0;i<11;i++)
+	{
+		min=crosses[i];
+		minat=i;
+		for(j=(i+1);j<11;j++)
+		{
+			if(min>crosses[j])
+			{
+				min=crosses[j];
+				minat=j;
+			}
+		}
+		temp=dims[i];
+		dims[i]=minat;
+		dims[minat]=temp;
+		crosses[minat]=crosses[i];
+		crosses[i]=min;
+	}
+}
 
 //@inputs starting ptr and current guesture
 //@output name of action if it exists NULL otherwise
 //checks the created actions for one that matches the given guesture
 int isAction(struct actionNode *ptr, int a[11])
 {
-	int i;
+	int i,j;
 	int outside;
         struct actionNode *ptr2=ptr;//for priority
         int h=0;
@@ -50,8 +106,9 @@ int isAction(struct actionNode *ptr, int a[11])
 		outside=0;
 		for(i=0;i<11;i++)//for each dimention
 		{
+			j=dims[i];
                  //check to see if the guesture matches the current action in the current dimention     
-			if((ptr->action->high[i]<a[i])||(ptr->action->low[i]>a[i]))
+			if((ptr->action->high[j]<a[j])||(ptr->action->low[j]>a[j]))
 			{
                                 //if it is mark guesture wrong and move on
 				outside=1;
@@ -90,42 +147,23 @@ struct actionNode * addAction(struct actionNode * ptr, int a[11], int name)
 	struct actionNode * rtn=ptr;
 	struct actionNode * ptr2;
 	int i;
-	/*
-	if(ptr==NULL)
-	{
-		ptr=(struct actionNode *)malloc(sizeof(struct actionNode *)); // Allocate space for next node
-		ptr->action=(struct action *)malloc(sizeof(struct action *)); // Allocate space for action struct
-		ptr->next=NULL;
-		ptr->action->high=(int*)malloc(22*sizeof(int));
-		ptr->action->low=&(ptr->action->high[11]);
-		// sets the highs and low for the action
-		for(i=0;i<11;i++)
-		{
-			ptr->action->low[i]=a[i]-1;
-			ptr->action->high[i]=a[i]+1;	
-		}
-		ptr->action->name=name;
-
-		return ptr;
-	}
-	*/
-	
 	//while pointer has data in it, update it
 	while(ptr!=NULL)
 	{
 		//checks if the name of the action is the same
 		if(ptr->action->name==name)
 		{
-                  Serial.println(name);
+			dimShift(ptr,-1);
 		  // goes through and sets the new max and min values
 		  for(i=0;i<11;i++)
 		  {
 		  	ptr->action->low[i]=min(ptr->action->low[i],a[i]-1);
 			ptr->action->high[i]=max(ptr->action->high[i],a[i]+1);
 		  }
-
+			dimShift(ptr,1);
 		  return rtn;
 		}
+		ptr2=ptr;
 		ptr=ptr->next;
 	}
 	// if there is no data instantiate the highs and lows  
@@ -140,7 +178,15 @@ struct actionNode * addAction(struct actionNode * ptr, int a[11], int name)
 		ptr->action->high[i]=a[i]+1;	
 	}
 	ptr->action->name=name;
-	ptr2->next=ptr;
+	if(rtn==NULL)
+	{
+		rtn=ptr;
+	}
+	else
+	{
+		ptr2->next=ptr;	
+	}
+	dimShift(ptr,1);
 	return rtn;
 }
 
@@ -159,13 +205,18 @@ int* recognize(int *array, int* avg) {
 		//If the difference is more than 3, break out of the method and take another input
 		if(abs(avg[k]-array[k])>3)
 		{
+			digitalWrite(10,LOW);
+  			digitalWrite(11,LOW);
+  			digitalWrite(12,LOW);
 			return avg;
 		}
 		
 	}
-	//isAction(peter,avg);
+	isAction(peter,avg);
 	return avg;
 }
+
+
 
 
 void setup() {
@@ -207,8 +258,5 @@ void loop() {
   readings[9]=analogRead(5);
     
   readings[10]=analogRead(0);
-  digitalWrite(10,LOW);
-  digitalWrite(11,LOW);
-  digitalWrite(12,LOW);
   p=recognize(readings, p);
 }
