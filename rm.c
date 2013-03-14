35983afec3a6e9ea93f6ee6b6bdf9e236992b91f
Siva <Zero@nbp-81-184.nbp.ruw.rutgers.edu>
2013-03-14 00:15:22 -0400

    again

--- rm.c
+++ rm.c
@@ -1,9 +1,18 @@
 void rmgesture(char* name) {
 	struct actionNode * point = peter;
+	struct actionNode * point2;
 	while(point != null || point->next->name != name) {
 		if (point->next->name == name)
-			point->next = point->next->next;
-
+		{
+			point2 = point;
+			point->next = point2->next->next;
+			free(point2->action->high);
+			free(point2->action);
+			free(point2);
+			return;
+		}
 		point = point->next;
 	}
+	
+
 }
