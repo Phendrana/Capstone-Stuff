11ce6e34726d662b37296c6cb8fa62cb0503207a
somewanabi <austinspadaro@gmail.com>
2013-04-10 11:55:23 -0300

    Create slave.c

---
+++ slave.c
@@ -0,0 +1,46 @@
+#include <SoftwareSerial.h>   
+
+ 
+SoftwareSerial blueToothSerial(RxD,TxD);
+ 
+void setup() 
+{ 
+  Serial.begin(9600);
+  pinMode(6, INPUT);
+  pinMode(7, OUTPUT);
+  setupBlueToothConnection();
+ 
+} 
+ 
+void loop() 
+{ 
+  char recvChar;
+  while(1){
+    if(blueToothSerial.available()){
+      recvChar = blueToothSerial.read();
+      Serial.print(recvChar);
+    }
+    if(Serial.available()){
+      recvChar  = Serial.read();
+      blueToothSerial.print(recvChar);
+    }
+  }
+} 
+ 
+void setupBlueToothConnection()
+{
+  blueToothSerial.begin(38400); 
+  blueToothSerial.print("\r\n+STWMOD=0\r\n"); 
+  blueToothSerial.print("\r\n+STNA=SeeedBTSlave\r\n"); 
+  blueToothSerial.print("\r\n+STOAUT=1\r\n"); 
+  blueToothSerial.print("\r\n+STAUTO=0\r\n"); e
+  delay(2000); 
+  blueToothSerial.print("\r\n+INQ=1\r\n");  
+  Serial.println("inquirable");
+  delay(2000); 
+  blueToothSerial.flush();
+}
+
+
+
+
